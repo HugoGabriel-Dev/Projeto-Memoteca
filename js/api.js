d1176c293b5dbf93390a2a5c2f7c07449e565630
HugoGabriel <hg116518@gmail.com>
2026-05-04 14:17:59 -0300

    criando novos pensamentos

--- js/api.js
+++ js/api.js
@@ -5,6 +5,23 @@ const api = {
       return await response.json();
     } catch {
       alert("Erro, não foi possível encontrar pensamento...");
+      throw Error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST", 
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json();
+    } catch {
+      alert("Erro, não foi possível encontrar pensamento...");
+      throw Error
     }
   },
 };
